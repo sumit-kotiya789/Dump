ab402510a5178c693885ec9bb357344972e50c2b
Sumit Kotiya <sumit.kotiya789@gmail.com>
2025-08-08 12:39:02 +0530

    Add files via upload

---
+++ opBNB/SC.sol
@@ -0,0 +1,142 @@
+/**
+ *Submitted for verification at OpbnbTrace on 2025-08-05
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+interface IBEP20 {
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function getOwner() external view returns (address);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address _owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract MyopBNBToken is IBEP20 {
+    string private _name = "opBNBtoken";
+    string private _symbol = "OPT";
+    uint8 private _decimals = 18;
+    uint256 private _totalSupply;
+
+    address private _owner;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    // Events
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    // Constructor
+    constructor() {
+        _owner = msg.sender;
+        _mint(msg.sender, 100000000 * 10 ** uint256(_decimals));
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    // Modifier
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Not the owner");
+        _;
+    }
+
+    // Required BEP-20 functions
+    function getOwner() public view override returns (address) {
+        return _owner;
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        uint256 currentAllowance = _allowances[sender][msg.sender];
+        require(currentAllowance >= amount, "BEP20: transfer amount exceeds allowance");
+        _approve(sender, msg.sender, currentAllowance - amount);
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    // Internal functions
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "Mint to zero address");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "Burn from zero address");
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "Burn exceeds balance");
+        _balances[account] = accountBalance - amount;
+        _totalSupply -= amount;
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0) && to != address(0), "Zero address");
+        require(_balances[from] >= amount, "Insufficient balance");
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0) && spender != address(0), "Zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    // Additional owner functions
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is zero");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
